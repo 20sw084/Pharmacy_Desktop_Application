99e410086c9d5e1217f39b606388ce5fabb05f3b
Junaid Aslam <junaidaslam.muet@gmail.com>
2022-04-06 05:58:08 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
-package Pharamacy_Management_System;
+package Pharmacy_Management_System;
 public class Main{
     public static void main(String[] args) {
-        new Pharm_Manag_Sys();
+    	new Pharm_Manag_Sys();
     }
 }
+
+/*
+Main requirements from Mam 
+1.similar Item in search
+2.reminder for fill the stock if stock is low
+3.stock to be bought will be proportional to product sold:
+ e.g best selling product will have a greater total stock
+*/
